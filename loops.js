c5910019bdb12f4acbb32a6bed200c97796dd5ae
geoffcraz <geoffcraz@gmail.com>
2017-10-10 14:57:55 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,14 +19,3 @@ function whileLoop(n) {
 }
 
 
-function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5
-  }
-
-  do {
-    array = array.slice(1)
-  } while (array.length > 0 && maybeTrue())
-
-  return array
-}
